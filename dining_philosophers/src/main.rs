e57eb1f552888cc9e32ae01053ef8733bd3a081f
Komei Nomura <komei.nomura@pepabo.com>
2018-05-19 10:01:41 +0900

    Philosopherを実装 - Philosopherの構造体を宣言 - 初期化メソッドnew追加 - eatメソッドを追加 - mainにPhilosoperのvectorを宣言 - Philosoperにeatメソッドを実行させた

--- dining_philosophers/src/main.rs
+++ dining_philosophers/src/main.rs
@@ -1,3 +1,31 @@
+#[derive(Debug)]
+struct Philosopher {
+    name: String,
+}
+
+impl Philosopher {
+    fn new(name: &str) -> Philosopher {
+        Philosopher {
+            name: name.to_string(),
+        }
+    }
+
+    fn eat(&self) {
+        println!("{} is done eating!", self.name);
+    }
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let philosophers = vec![
+        Philosopher::new("John"),
+        Philosopher::new("Andy"),
+        Philosopher::new("Karl"),
+        Philosopher::new("Emma"),
+        Philosopher::new("Michel"),
+    ];
+
+    for p in &philosophers {
+        p.eat();
+    }
 }
